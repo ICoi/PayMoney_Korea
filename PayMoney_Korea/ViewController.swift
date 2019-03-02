640bb03387860b5b25a769a0343b00d1a4d28b1d
daunjoung <ICo1022+github@gmail.com>
2019-03-02 13:51:29 +0900

    Initial Commit

---
+++ PayMoney_Korea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayMoney_Korea
+//
+//  Created by 정다운 on 2019. 3. 2..
+//  Copyright © 2019년 daunjoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
